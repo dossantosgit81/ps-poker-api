ebe6df095a734b92221118c77721691bcb832154
Rafael Mendes <dossantosgit81@gmail.com>
2025-06-23 13:16:26 -0300

    Edit cards

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+/*  <ion-card-header>
+      <ion-card-title>Card Title</ion-card-title>
+      <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+    </ion-card-header>
+
+    <ion-card-content> */
+import { IonHeader, 
+  IonToolbar, 
+  IonTitle, 
+  IonContent, 
+  IonCard, 
+  IonCardHeader, 
+  IonCardTitle, 
+  IonCardSubtitle, 
+  IonCardContent,
+  IonGrid,
+  IonRow,
+  IonCol } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, 
+    IonToolbar, 
+    IonTitle, 
+    IonContent, 
+    IonCard, 
+    IonCardHeader, 
+    IonCardTitle, 
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonCardSubtitle, 
+    IonCardContent ],
 })
 export class HomePage {
   constructor() {}
