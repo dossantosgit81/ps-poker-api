a6c2a20ac56f7a374924a0f43767861a0f7c3544
Rafael <dossantosgit81@gmail.com>
2025-06-27 11:07:09 -0300

    create page details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { PokemonsService } from '../services/pokemons.service';
+import { 
+  IonContent, 
+ } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss'],
+  imports: [IonContent]
+  
 })
 export class DetailsComponent  implements OnInit {
   private pokemonsService = inject(PokemonsService);
@@ -15,8 +20,10 @@ export class DetailsComponent  implements OnInit {
   ngOnInit() {
     const url = this.route.snapshot.paramMap.get('url');
     if(url !== null){
-      this.pokemonDetail = this.pokemonsService.getPokemon(this.getIdPokemon(url)).subscribe(res => {
+       this.pokemonsService.getPokemon(this.getIdPokemon(url)).subscribe(res => {
         console.log(res);
+        console.log(res.abilities[0].ability.name)
+        this.pokemonDetail = res;
       });
     }
   }
@@ -24,7 +31,6 @@ export class DetailsComponent  implements OnInit {
   getIdPokemon(url: string){
     const parts = url.split("/");
     const id = parts[parts.length - 2]; 
-    console.log(id); // "1"
     return id;
   }
 
